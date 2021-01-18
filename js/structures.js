80cef97f42d3697a3d83cd1d10858ac58759f483
IvanWiesner <ivanwiesner@gmail.com>
2021-01-17 18:23:28 -0700

    added description
    description

--- js/structures.js
+++ js/structures.js
@@ -1,26 +1,26 @@
 var allWorkContent = [
     {
-        title: 'kitchen cook',
-        dates: 'January 2020 - May 2021',
-        description: 'I like to cook :)',
+        title: 'Banquet Employee',
+        dates: 'October 2014 - May 2015',
+        description: 'Responsibilities included setting up projectors/ tech equipment, customer service to solve problems, and recruit people to join the staff. Very capable in addressing stressful situations calmly and dealing with difficult customers. Assisted in various tasks in opening for new events, while being collaborative and compatible with many areas including management. Personality was highly effective in increasing sales and customer retention while successfully managing full time schooling hours.',
         img: ''
     },
     {
-        title: 'kitchen cook',
-        dates: 'January 2020 - May 2021',
-        description: 'I like to cook :)',
+        title: 'Line cook',
+        dates: 'May 2015 - May 2017',
+        description: 'Responsibilities included recruiting and training new employees, closing duties, re-stocking of supplies. Initiated a productive atmosphere in the kitchen - keeping everyone in line and in sequential order. Ticker master/Multi tasker',
         img: ''
     },
     {
-        title: 'kitchen cook',
-        dates: 'January 2020 - May 2021',
-        description: 'I like to cook :)',
+        title: 'Denver Drake Jungler',
+        dates: 'May 2017 - October 2018',
+        description: 'Joined semi-pro teams for going pro in league of legends, to see what it would be like.',
         img: ''
     },
     {
-        title: 'kitchen cook',
-        dates: 'January 2020 - May 2021',
-        description: 'I like to cook :)',
+        title: 'Streamer',
+        dates: 'January 2019 - October 2020',
+        description: 'Streamed part time while I went to school instead of working because it was more enjoyable to do.',
         img: ''
     }
 ];
