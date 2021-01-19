166890e9696df113f909398cc20846d375d59db6
IvanWiesner <ivanwiesner@gmail.com>
2021-01-19 11:37:19 -0700

    pics

--- js/main.js
+++ js/main.js
@@ -40,7 +40,12 @@ function initImg() {
     Img.classList.add('Img');
     Img.innerHTML = 
     "<div>" +
-    "<Img src='../face.jpg'>" +
+    "<Img src='../hotel.jpg' id='hotel'>" +
+    "<Img src='../face.jpg' id='face'>" +
+    "<Img src='../cook.PNG' id='cook'>" +
+    "<Img src='../jungle.PNG' id='jungle'>" +
+    "<Img src='../drakes.PNG' id='drakes'>" +
+    "<Img src='../stream.Png' id='stream'>" +
     "</div>";
     document.body.appendChild(Img);
 }
