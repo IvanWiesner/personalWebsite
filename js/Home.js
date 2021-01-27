05d2b193447f3cc4c0b908d457b463c7334f21e2
IvanWiesner <ivanwiesner@gmail.com>
2021-01-26 17:13:23 -0700

    Update Home.js

--- js/Home.js
+++ js/Home.js
@@ -1,7 +1,28 @@
 document.body.addEventListener('load', initHead(), false);
 
-createWorkContent();
+function createWorkContent() {
 
-    funtcion initHead () {
+    var workContainer = document.createElement('div');
+    workContainer.id = 'home-Content';
 
-    }
+        for (var i = 0; i < allWorkContent.length; i++) { 
+            var workElement = document.createElement('div');
+        workElement.classList.add('work-element');
+        workElement.innerHTML = 
+        '<div class="left-work-element">' +
+                '<h2>' + allWorkContent[i].schooling + '</h2>' +
+                '<p>' + allWorkContent[i].extracurricular + '</p>' + 
+            '</div>' + 
+            '<div class="right-work-element">' + 
+                '<img src="' + allWorkContent[i].img + '" />' + 
+            '</div>';
+            var allWorkContent = [
+                {
+                    classes took: 'Business, maths, english, science, emt'
+                    dates attended: '2018-2020'
+                    schools: 'University of Colorado Colorado Springs and Front Range Community College'
+                },
+                    band: ''
+                    deca: ''
+                    achievements: ''
+            };
