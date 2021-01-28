6c9134a8327f986fb5c06c58e4ecae29b717cafc
IvanWiesner <ivanwiesner@gmail.com>
2021-01-27 19:35:00 -0700

    new folders

---
+++ js/aboutme.js
@@ -0,0 +1,17 @@
+function createWorkContent() {
+
+    var workContainer = document.createElement('div');
+    workContainer.id = 'home-Content';
+
+        for (var i = 0; i < allWorkContent.length; i++) { 
+            var workElement = document.createElement('div');
+        workElement.classList.add('work-element');
+        workElement.innerHTML = 
+        '<div class="left-work-element">' +
+                '<h2>' + allWorkContent[i].schooling + '</h2>' +
+                '<p>' + allWorkContent[i].extracurricular + '</p>' + 
+            '</div>' + 
+            '<div class="right-work-element">' + 
+                '<img src="' + allWorkContent[i].img + '" />' + 
+            '</div>';
+            var allWorkContent
