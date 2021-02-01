8ddaf5c6bf3e987f812ba88ffbcf255304604da4
IvanWiesner <ivanwiesner@gmail.com>
2021-02-01 11:24:24 -0700

    Create pratics.js

---
+++ js/pratice/pratics.js
@@ -0,0 +1,24 @@
+for (let i = 0 initial expression)
+for (let i = 0; condition)
+for (let i = 0; i < 5; incremint expression)
+for (let i = 0; i < 5; i++) {
+    // display 'hello world on the console//
+    console.log('Hello World');
+    // 5 hello world messages//
+}
+//the conditoin is evaluated and because the condition is true the condition is executed//
+console.log('Hello World', i); 
+    //Hello World 0
+    //Hello World 1
+    //2
+    //3
+    //4
+for (let i =1; i <= 5; i++) {
+
+}
+console.log('Hello World')
+    //Hello World 1
+    //Hello World 2
+    //3
+    //4
+    //5
